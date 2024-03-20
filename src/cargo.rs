519ab48d78c33f2482016b09f3d555b5c188bcc4
Taiki Endo <te316e89@gmail.com>
2024-03-20 17:15:44 +0900

    Refer cargo version instead of rustc version
    See https://docs.rs/cargo-config2/0.1/cargo_config2/struct.Config.html#method.rustc_version for more.

--- src/cargo.rs
+++ src/cargo.rs
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use std::{
-    env,
-    path::{Path, PathBuf},
-};
+use std::{env, ffi::OsStr, path::PathBuf};
 
 use anyhow::{format_err, Result};
 
@@ -24,12 +21,11 @@ enum CargoMode {
 impl Workspace {
     pub(crate) fn new(manifest_path: Option<&str>) -> Result<Self> {
         let cargo = env::var_os("CARGO").unwrap_or_else(|| "cargo".into());
-        let rustc = rustc_path(&cargo);
-        let rustc_version = rustc_version(&rustc)?;
+        let cargo_version = cargo_version(&cargo)?;
 
-        let metadata = metadata::Metadata::new(manifest_path, &cargo, rustc_version.minor)?;
+        let metadata = metadata::Metadata::new(manifest_path, &cargo, cargo_version.minor)?;
 
-        let cargo_mode = if rustc_version.nightly {
+        let cargo_mode = if cargo_version.nightly {
             CargoMode::Nightly
         } else if cmd!("rustup", "run", "nightly", "cargo", "--version").run_with_output().is_ok() {
             // Favor `rustup run nightly cargo update -Z ...` over `RUSTC_BOOTSTRAP=1 cargo update -Z ...`
@@ -61,35 +57,20 @@ impl Workspace {
     }
 }
 
-fn rustc_path(cargo: impl AsRef<Path>) -> PathBuf {
-    // When toolchain override shorthand (`+toolchain`) is used, `rustc` in
-    // PATH and `CARGO` environment variable may be different toolchains.
-    // When Rust was installed using rustup, the same toolchain's rustc
-    // binary is in the same directory as the cargo binary, so we use it.
-    let mut rustc = cargo.as_ref().to_owned();
-    rustc.pop(); // cargo
-    rustc.push(format!("rustc{}", env::consts::EXE_SUFFIX));
-    if rustc.exists() {
-        rustc
-    } else {
-        "rustc".into()
-    }
-}
-
-fn rustc_version(rustc: &Path) -> Result<RustcVersion> {
+fn cargo_version(cargo: &OsStr) -> Result<CargoVersion> {
     // Use verbose version output because the packagers add extra strings to the normal version output.
-    let mut cmd = cmd!(rustc, "--version", "--verbose");
+    let mut cmd = cmd!(cargo, "--version", "--verbose");
     let verbose_version = cmd.read()?;
-    RustcVersion::parse(&verbose_version)
+    CargoVersion::parse(&verbose_version)
         .ok_or_else(|| format_err!("unexpected version output from {cmd}: {verbose_version}"))
 }
 
-struct RustcVersion {
+struct CargoVersion {
     minor: u32,
     nightly: bool,
 }
 
-impl RustcVersion {
+impl CargoVersion {
     fn parse(verbose_version: &str) -> Option<Self> {
         let release = verbose_version.lines().find_map(|line| line.strip_prefix("release: "))?;
         let (version, channel) = release.split_once('-').unwrap_or((release, ""));
@@ -99,7 +80,7 @@ impl RustcVersion {
             return None;
         }
         let minor = digits.next()?.parse::<u32>().ok()?;
-        let _patch = digits.next().unwrap_or("0").parse::<u32>().ok()?;
+        let _patch = digits.next()?.parse::<u32>().ok()?;
         let nightly = channel == "nightly"
             || channel == "dev"
             || env::var("RUSTC_BOOTSTRAP").ok().as_deref() == Some("1");
